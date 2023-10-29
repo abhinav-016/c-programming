a3914c2767cef322bd80ff124604bafc47af8159
abhi00016 <abhichaurasia016@gmail.com>
2023-10-29 15:09:45 +0530

    c coding

---
+++ input two numbers and print sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("ENTER TWO NUMBERS");
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    printf("SUM OF TWO NUMBERS %d",c);
+    return 0;
+}
+    
---
+++ sum of two numbers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    a=7;
+    b=9;
+    c=a+b;
+    printf("sum of two numbers %d",c);
+}
