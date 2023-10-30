4d0cfa729e268bb41efd027c2ec4a19119e5bfd3
abhi00016 <abhichaurasia016@gmail.com>
2023-10-30 22:13:15 +0530

    new c files

---
+++ areaofrectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a,l,b;
+    printf("ENTER LENGTH: ");
+    scanf("%d", &l);
+    printf("ENTER BREADTH: ");
+    scanf("%d", &b);
+    a=l*b;
+    printf("AREA OF RECTANGLE: %d", a);
+}
